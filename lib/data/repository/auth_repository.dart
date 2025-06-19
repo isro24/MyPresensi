13106a30fa2a759de91ca4b4fb2a3040d2a7c046
isro24 <alamat_email_anda@example.com>
2025-06-19 18:20:03 +0700

    Menambah AuthRepository pada repository

---
+++ lib/data/repository/auth_repository.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:my_presensi/data/models/request/auth/login_request_model.dart';
+import 'package:my_presensi/data/models/response/auth/login_response_model.dart';
+import 'package:my_presensi/service/service_http_client.dart';
+
+class AuthRepository {
+  final ServiceHttpClient _serviceHttpClient;
+  final secureStorage = FlutterSecureStorage();
+
+  AuthRepository(this._serviceHttpClient);
+
+  Future<Either<String, LoginResponseModel>> login(
+    LoginRequestModel requestModel,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'login',
+        requestModel.toMap(),
+      );
+
+      log(response.body);
+      
+      final jsonResponse = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final loginResponse = LoginResponseModel.fromMap(jsonResponse);
+        await secureStorage.write(
+          key: 'token',
+          value: loginResponse.data!.token,
+        );
+        await secureStorage.write(
+          key: 'userRole',
+          value: loginResponse.data!.role,
+        );
+        return Right(loginResponse);
+      } else {
+        return Left(jsonResponse['message'] ?? 'Login failed');
+      }
+    } catch (e) {
+      return left('An error occured while logging in.');
+    }
+  }
+}
