39a795ecd93050119802f4defe1d0f6d6ff2e034
isro24 <alamat_email_anda@example.com>
2025-06-26 03:12:53 +0700

    Menambah DashboardRepository pada repository

---
+++ lib/data/repository/dashboard_repository.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:my_presensi/data/models/response/employee/dashboard_response_model.dart';
+import 'package:my_presensi/service/service_http_client.dart';
+
+class DashboardRepository {
+  final ServiceHttpClient _serviceHttpClient;
+
+  DashboardRepository(this._serviceHttpClient);
+
+  Future<Either<String, DashboardResponseModel>> getDashboardData() async {
+    try {
+      final response = await _serviceHttpClient.get("employee/dashboard");
+
+      log("Dashboard status: ${response.statusCode}");
+      log("Dashboard body: ${response.body}");
+
+      if (response.statusCode == 200) {
+        final jsonResponse = json.decode(response.body);
+        final dashboardResponse = DashboardResponseModel.fromJson(jsonResponse);
+        print("Profile Response: $dashboardResponse");
+        return Right(dashboardResponse);
+      } else {
+        final errorMessage = json.decode(response.body);
+        return Left(errorMessage['message'] ?? 'Unknown error occurred');
+      }
+    } catch (e) {
+      return Left("An error occurred while fetching dashboard data: $e");
+    }
+  }
+}
