cac08f9b247e1bd1a1386cbfff1a834eb75f8e83
isro24 <alamat_email_anda@example.com>
2025-06-26 03:11:09 +0700

    Menambah file int_ext dan export pada extension

---
+++ lib/core/extension/int_ext.dart
@@ -0,0 +1,34 @@
+import 'package:intl/intl.dart';
+
+extension DateTimeExt on DateTime {
+  String get formatTanggal {
+    const hariIndo = [
+      'Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'
+    ];
+    const bulanIndo = [
+      '', 'Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni',
+      'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'
+    ];
+
+    final hari = hariIndo[weekday % 7]; 
+    return '$hari, $day ${bulanIndo[month]} $year';
+  }
+
+  String get formatJam {
+    final jam = hour.toString().padLeft(2, '0');
+    final menit = minute.toString().padLeft(2, '0');
+    return '$jam:$menit WIB';
+  }
+}
+
+extension TimeFormatExt on String {
+  String get formatJamWIB {
+    try {
+      final parsedTime = DateFormat("HH:mm:ss").parse(this);
+      return '${DateFormat("HH.mm").format(parsedTime)} WIB';
+    } catch (e) {
+      return this;
+    }
+  }
+}
+
