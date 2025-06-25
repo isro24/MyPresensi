5b31e47657dcb50d0b2a7078ecfc1dd5f2ea91a7
isro24 <alamat_email_anda@example.com>
2025-06-26 03:14:00 +0700

    MenambahDashboardState pada dashboard_bloc

---
+++ lib/presentation/employee/dashboard/dashboard_bloc/dashboard_employee_state.dart
@@ -0,0 +1,19 @@
+part of 'dashboard_employee_bloc.dart';
+
+sealed class DashboardState {}
+
+final class DashboardInitial extends DashboardState {}
+
+final class DashboardLoading extends DashboardState {}
+
+final class DashboardLoaded extends DashboardState {
+  final DashboardResponseModel dashboard;
+
+  DashboardLoaded({required this.dashboard});
+}
+
+final class DashboardError extends DashboardState {
+  final String message;
+
+  DashboardError({required this.message});
+}
