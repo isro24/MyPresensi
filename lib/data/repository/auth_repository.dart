6256cd7bcdfe9774e9395f10a2673d90200b58f7
isro24 <alamat_email_anda@example.com>
2025-07-14 02:09:18 +0700

    Menambah Forgot dan Reset Password pada AuthRepisotory

--- lib/data/repository/auth_repository.dart
+++ lib/data/repository/auth_repository.dart
@@ -3,7 +3,9 @@ import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:my_presensi/data/models/request/auth/forgot_password_request_model.dart';
 import 'package:my_presensi/data/models/request/auth/login_request_model.dart';
+import 'package:my_presensi/data/models/request/auth/reset_password_request_model.dart';
 import 'package:my_presensi/data/models/response/auth/login_response_model.dart';
 import 'package:my_presensi/service/service_http_client.dart';
 
@@ -68,4 +70,48 @@ class AuthRepository {
     await secureStorage.delete(key: 'userRole');
   }
 
+  // Forgot Password
+  Future<Either<String, Map<String, String>>> forgotPassword(
+  ForgotPasswordRequestModel model,
+  ) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'forgot-password',
+        model.toMap(),
+      );
+      final jsonData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        final data = jsonData['data'];
+        return Right({
+          'email': data['email'],
+          'token': data['token'],
+          'message': jsonData['message']
+        });
+      } else {
+        return Left(jsonData['message'] ?? 'Gagal mengirim token reset');
+      }
+    } catch (e) {
+      return Left('Error sending reset token: $e');
+    }
+  }
+
+
+  // Reset Password
+  Future<Either<String, String>> resetPassword(
+    ResetPasswordRequestModel model) async {
+    try {
+      final response = await _serviceHttpClient.post(
+        'reset-password',
+        model.toMap(),
+      );
+      final jsonData = json.decode(response.body);
+      if (response.statusCode == 200) {
+        return Right(jsonData['message'] ?? 'Reset password berhasil');
+      } else {
+        return Left(jsonData['message'] ?? 'Reset password gagal');
+      }
+    } catch (e) {
+      return Left('Error resetting password: $e');
+    }
+  }
 }
